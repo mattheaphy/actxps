237bb9c98fa428d9ea6f986722f4a6f5b9e8192d
Matt Heaphy <matt.heaphy@gmail.com>
2022-06-23 23:35:06 -0400

    Expanded documentation of the expose function and added efficiencies.

--- R/expose.R
+++ R/expose.R
@@ -1,42 +1,64 @@
 #' Create exposure records from census records
 #'
-#' @description update me
+#' @description Convert a data frame of census-level records to exposure-level
+#' records.
 #'
-#' @details add details here
+#' @details Census-level data refers to a data set wherein there is one row
+#' per unique policy. Exposure-level data expands census-level data such that
+#' there is one record per policy per observation period. Observation periods
+#' could be any meaningful period of time such as a policy year, policy month,
+#' calendar year, calendar quarter, calendar month, etc.
+#'
+#' \code{target_status} is used in the calculation of exposures. The annual
+#' exposure method is applied, which allocates a full period of exposure for
+#' any statuses in \code{target_status}. For all other statuses, new entrants
+#' and exits are partially exposed based on the time elapsed in the observation
+#' period. This method is consistent with the Balducci Hypothesis, which assumes
+#' that the probability of termination is proportionate to the time elapsed
+#' in the observation period. If the annual exposure method isn't desired,
+#' \code{target_status} can be ignored. In this case, partial exposures are
+#' always applied regardless of status.
 #'
 #' @param .data a data frame with census-level records
 #' @param end_date experience study end date
-#' @param start_date experience study start date
-#' @param target_status character vector of target status values
+#' @param start_date experience study start date. Default value = 1900-01-01.
+#' @param target_status character vector of target status values. Default value = \code{NULL.}
 #' @param col_pol_num name of the column in \code{.data} containing the policy number
 #' @param col_status name of the column in \code{.data} containing the policy status
 #' @param col_issue_date name of the column in \code{.data} containing the issue date
 #' @param col_term_date name of the column in \code{.data} containing the termination date
 #'
-#' @return something useful
+#' @return A tibble with class \code{exposed_df}, \code{tbl_df}, \code{tbl},
+#' and \code{data.frame}. The results include all existing columns in
+#' \code{.data} plus new columns for exposures and the observation period.
 #'
 #' @import rlang
 #'
 #' @examples
 #' toy_census |> expose("2020-12-31", target_status = "Surrender")
 #'
+#' \dontrun{
+#' census_dat |>
+#'     expose("2019-12-31", target_status = "Surrender")}
+
+#'
+#' @references https://www.soa.org/49378a/globalassets/assets/files/research/experience-study-calculations.pdf
+#'
 #' @export
 expose <- function(.data,
-                   end_date,
-                   start_date,
-                   target_status = "None",
-                   col_pol_num = "pol_num",
-                   col_status = "status",
-                   col_issue_date = "issue_date",
-                   col_term_date = "term_date") {
+                    end_date,
+                    start_date = as.Date("1900-01-01"),
+                    target_status = NULL,
+                    col_pol_num = "pol_num",
+                    col_status = "status",
+                    col_issue_date = "issue_date",
+                    col_term_date = "term_date") {
 
   .data <- .data |>
     dplyr::rename(pol_num = {{col_pol_num}},
-           status = {{col_status}},
-           issue_date = {{col_issue_date}},
-           term_date = {{col_term_date}})
-
-  if(missing(start_date)) start_date <- as.Date("1900-01-01")
+                  status = {{col_status}},
+                  issue_date = {{col_issue_date}},
+                  term_date = {{col_term_date}})
 
   if(!is.factor(.data$status)) .data$status <- factor(.data$status)
 
@@ -47,8 +69,9 @@ expose <- function(.data,
                   is.na(term_date) | term_date > start_date) |>
     dplyr::mutate(
       last_date = pmin(term_date, end_date, na.rm = TRUE),
-      pol_yr = lubridate::interval(issue_date, last_date) %/%
-                    lubridate::years(1) + 1) |>
+      tot_int = lubridate::interval(issue_date, last_date),
+      pol_yr = tot_int %/% lubridate::years(1) + 1,
+      tot_yrs = tot_int / lubridate::years(1)) |>
     dplyr::slice(rep(dplyr::row_number(), pol_yr)) |>
     dplyr::group_by(pol_num) |>
     dplyr::mutate(
@@ -56,14 +79,11 @@ expose <- function(.data,
       pol_yr = dplyr::row_number()) |>
     dplyr::ungroup() |>
     dplyr::mutate(
-      exposure =
-        ifelse(last_yr & !status %in% target_status,
-               (lubridate::interval(issue_date, last_date) /
-                  lubridate::years(1)) %% 1, 1),
+      exposure = ifelse(last_yr & !status %in% target_status, tot_yrs %% 1, 1),
       status = dplyr::if_else(last_yr, status, default_status),
       term_date = dplyr::if_else(last_yr, term_date, lubridate::NA_Date_)
     ) |>
-    dplyr::select(-last_yr, -last_date)
+    dplyr::select(-last_yr, -last_date, -tot_yrs, -tot_int)
 
   structure(res, class = c("exposed_df", class(res)),
             target_status = target_status,
