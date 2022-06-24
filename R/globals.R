b1606bf1f5c4fdec2cc8b026e1d4f786b1a777df
Matt Heaphy <matt.heaphy@gmail.com>
2022-06-23 23:36:35 -0400

    Added more global vars

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,3 @@
 utils::globalVariables(c("issue_date", "term_date", "last_date", "pol_yr",
                          "pol_num", "last_yr", "status", "term",
-                         "exposure", "claims"))
+                         "exposure", "claims", "tot_int", "tot_yrs"))
