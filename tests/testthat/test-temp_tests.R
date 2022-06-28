d15f63ff1c1a4e7e50678feb9528f946bf9c1e62
Matt Heaphy <matt.heaphy@gmail.com>
2022-06-28 07:38:52 -0400

    test update

--- tests/testthat/test-temp_tests.R
+++ tests/testthat/test-temp_tests.R
@@ -1,3 +1,14 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+study_py <- expose(census_dat, "2019-12-31")
+study_cy <- expose_cy(census_dat, "2019-12-31")
+
+test_that("py_exposure_checks", {
+  expect_gt(min(study_py$exposure), 0)
+  expect_lte(max(study_py$exposure), 1)
+  expect_equal(sum(is.na(study_py$exposure)), 0)
+})
+
+test_that("cy_exposure_checks", {
+  expect_gt(min(study_cy$exposure), 0)
+  expect_lte(max(study_cy$exposure), 1)
+  expect_equal(sum(is.na(study_cy$exposure)), 0)
 })
