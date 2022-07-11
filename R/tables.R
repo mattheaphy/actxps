a754902750bcf5fe7689898548136c08f54dd904
Matt Heaphy <matt.heaphy@gmail.com>
2022-07-11 06:54:00 -0400

    1st draft of autotable S3 generic

---
+++ R/tables.R
@@ -0,0 +1,35 @@
+#' Tabular experience study summary
+#'
+#' @description \code{autotable} is a generic function used to create a table
+#' from an object of a particular class. Tables are constructed using the
+#' \code{gt} package.
+#'
+#' \code{autotable.exp_df} is used to convert experience study results to a
+#' presentation-friendly format.
+
+#'
+#' @param object An object of class \code{exp_df} usually created by the
+#' function \code{exp_stats()}.
+#' @param fontsize Font size percentage multiplier.
+#' @param ... Not used
+#'
+#'
+#' @return a \code{gt} object
+#'
+#' @export
+autotable <- function(object, ...) {
+  UseMethod("autotable")
+}
+
+#' @rdname autotable
+#' @export
+autotable.exp_df <- function(object, fontsize = 100, ...) {
+
+  object |>
+    gt::gt() |>
+    gt::fmt_number(c(claims, exposure), decimals = 0) |>
+    gt::fmt_percent(starts_with("q_"), decimals = 1) |>
+    gt::fmt_percent(starts_with("ae_"), decimals = 1) |>
+    gt::tab_options(table.font.size = gt::pct(fontsize))
+
+}
