7f48f0d9fc748fa1f3df442c8e15da9c49547f6f
Matt Heaphy <matt.heaphy@gmail.com>
2022-07-19 00:05:20 -0400

    autotable formatting update

--- R/tables.R
+++ R/tables.R
@@ -42,21 +42,33 @@ autotable.exp_df <- function(object, fontsize = 100, decimals = 1,
 
   expected <- attr(object, "expected")
   target_status <- attr(object, "target_status")
+  wt <- attr(object, "wt")
 
   tab <- object |>
     gt::gt(...) |>
     gt::fmt_number(c(claims, exposure), decimals = 0) |>
     gt::fmt_percent(c(q_obs,
                       dplyr::starts_with("ae_"),
+                      dplyr::starts_with("adj_"),
+                      dplyr::any_of("credibility"),
                       expected),
                     decimals = decimals) |>
     gt::tab_options(table.font.size = gt::pct(fontsize),
                     row.striping.include_table_body = TRUE) |>
     gt::tab_style(list(gt::cell_text(weight = "bold")),
                   locations = gt::cells_column_labels()) |>
+    gt::cols_label(q_obs = gt::md("*q<sup>obs</sup>*")) |>
     gt::tab_header(title = "Experience Study Results",
                    subtitle = glue::glue("Target status{ifelse(length(target_status) > 1,'es','')}: {paste(target_status, collapse = ', ')}")) |>
     gt::tab_source_note(glue::glue("Study range: {as.character(attr(object, 'start_date'))} to {as.character(attr(object, 'end_date'))}"))
+
+  if (length(wt) > 0) {
+    tab <- tab |>
+      gt::tab_source_note(glue::glue("Results weighted by {wt}"))
+  } else {
+    tab <- tab |> gt::cols_hide(n_claims)
+  }
+
 
   for (i in expected) {
     tab <- tab |> span_expected(i)
@@ -66,9 +78,7 @@ autotable.exp_df <- function(object, fontsize = 100, decimals = 1,
     tab <- tab |>
       gt::tab_style(list(gt::cell_text(weight = "bold")),
                     locations = gt::cells_column_spanners())
-
   }
-
   if (colorful) {
     tab <- tab |>
       gt::data_color(
@@ -98,7 +108,8 @@ span_expected <- function(tab, ex) {
 
   force(ex)
   tab |>
-    gt::tab_spanner(ex, c(ex, paste0("ae_", ex))) |>
+    gt::tab_spanner(ex, c(ex, paste0("ae_", ex), paste0("adj_", ex))) |>
     gt::cols_label(!!rlang::enquo(ex) := gt::md("E[*X*]"),
-                   !!rlang::sym(paste0("ae_", ex)) := "A/E")
+                   !!rlang::sym(paste0("ae_", ex)) := "A/E",
+                   !!rlang::sym(paste0("adj_", ex)) := gt::md("*q<sup>adj</sup>*"))
 }
