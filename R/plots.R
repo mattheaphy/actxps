98d0b4c71858f0dfd974bb74bde86bcd6afec25a
Matt Heaphy <matt.heaphy@gmail.com>
2022-08-19 23:03:40 -0400

    New geometry argument for autoplot.exp_df method

--- R/plots.R
+++ R/plots.R
@@ -5,6 +5,8 @@
 #' @param ... Faceting variables passed to `facet_wrap()`.
 #' @param mapping Aesthetic mapping passed to `ggplot()`.
 #' @param scales The `scales` argument passed to `facet_wrap()`.
+#' @param geoms Type of geometry. If "points" is passed, the plot will
+#' display lines and points. If "bars", the plot will display bars.
 #'
 #' @details If no aesthetic map is supplied, the plot will use the first
 #' grouping variable in `object` on the x axis and `q_obs` on the y
@@ -17,12 +19,18 @@
 #' @return a `ggplot` object
 #'
 #' @export
-autoplot.exp_df <- function(object, ..., mapping, scales = "fixed") {
+autoplot.exp_df <- function(object, ..., mapping, scales = "fixed",
+                            geoms = c("lines", "bars")) {
 
   .groups <- attr(object, "groups")
-  n_groups <- length(.groups)
+  if(length(.groups) == 0) {
+    .groups <- list(rlang::expr(All))
+    object$All <- ""
+  }
 
   defaultNULL <- function(x) if (length(.groups) < x) NULL else .groups[[x]]
+
+  geoms <- match.arg(geoms)
 
   # set up aesthetics
   if(missing(mapping)) {
@@ -40,9 +48,14 @@ autoplot.exp_df <- function(object, ..., mapping, scales = "fixed") {
   }
 
   p <- ggplot2::ggplot(object, mapping) +
-    ggplot2::geom_point() +
-    ggplot2::geom_line() +
     ggplot2::scale_y_continuous(labels = scales::label_percent(accuracy = 0.1))
+
+  if (geoms == "lines") {
+    p <- p + ggplot2::geom_point() + ggplot2::geom_line()
+  } else {
+    p <- p + ggplot2::geom_col(position = "dodge")
+  }
+
 
   if (is.null(facet)) return(p)
   p + ggplot2::facet_wrap(ggplot2::vars(!!!facet), scales = scales)
