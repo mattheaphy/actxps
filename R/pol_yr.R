d8a0d140ee7bb600ae0b92bc43a84e145012e788
Matt Heaphy <matt.heaphy@gmail.com>
2023-02-06 07:08:53 -0500

    documentation update

--- R/pol_yr.R
+++ R/pol_yr.R
@@ -1,15 +1,19 @@
-#' Calculate policy durations
+#' Calculate policy duration
 #'
 #' @description Given a vector of dates and a vector of issue dates, calculate
-#' policy years, quarters, months, or weeks.
+#' policy years, quarters, months, weeks, or other duration.
 #'
-#' @details This function assumes the first day of each policy year is the
+#' @details These functions assume the first day of each policy year is the
 #' anniversary date (or issue date in the first year). The last day of each
 #' policy year is the day before the next anniversary date. Analogous rules
 #' are used for policy quarters, policy months, and policy weeks.
 #'
+#' The [pol_interval()] function can be used to determine any arbitrary
+#' duration passed to the `expo_length` argument.
+#'
 #' @param x A vector of dates
 #' @param issue_date A vector of issue dates
+#' @param expo_length A period object. See [lubridate::period()].
 #'
 #' @return An integer vector
 #'
@@ -19,11 +23,6 @@
 #' pol_mth(as.Date("2021-02-28") + 0:2, "2020-02-29")
 #'
 #' @name pol_yr
-
-pol_interval <- function(x, issue_date, expo_length) {
-  ceiling(lubridate::interval(as.Date(issue_date) - 1, x) / expo_length)
-}
-
 #' @rdname pol_yr
 #' @export
 pol_yr <- function(x, issue_date) {
@@ -47,3 +46,10 @@ pol_mth <- function(x, issue_date) {
 pol_wk <- function(x, issue_date) {
   pol_interval(x, issue_date, lubridate::days(7))
 }
+
+#' @rdname pol_yr
+#' @export
+pol_interval <- function(x, issue_date, expo_length) {
+  ceiling(lubridate::interval(as.Date(issue_date) - 1, x) / expo_length)
+}
+
