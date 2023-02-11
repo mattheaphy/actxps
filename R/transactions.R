f3f6383126e55204c08cee5f5039f48f1b392647
Matt Heaphy <matt.heaphy@gmail.com>
2023-02-11 08:19:23 -0500

    Added column rename options for the transactions data frame

--- R/transactions.R
+++ R/transactions.R
@@ -9,6 +9,14 @@
 #' `exposed_df`. Use [as_exposed_df()] to convert a data frame to an
 #' `exposed_df` object if necessary.
 #' @param trx_data a data frame containing transactions details
+#' @param col_pol_num name of the column in `trx_data` containing the policy
+#' number
+#' @param col_trx_date name of the column in `trx_data` containing the
+#' transaction date
+#' @param col_trx_type name of the column in `trx_data` containing the
+#' transaction type
+#' @param col_trx_amt name of the column in `trx_data` containing the
+#' transaction amount
 #'
 #' @examples
 #' expo <- expose_py(census_dat, "2019-12-31", target_status = "Surrender")
@@ -19,7 +27,11 @@
 #' @importFrom dplyr between
 #'
 #' @export
-add_transactions <- function(.data, trx_data) {
+add_transactions <- function(.data, trx_data,
+                             col_pol_num = "pol_num",
+                             col_trx_date = "trx_date",
+                             col_trx_type = "trx_type",
+                             col_trx_amt = "trx_amt") {
 
   if(!is_exposed_df(.data)) {
     rlang::abort(c(x = "`.data` must be an `exposed_df` object.",
@@ -29,16 +41,23 @@ add_transactions <- function(.data, trx_data) {
 
   date_cols <- attr(.data, "date_cols") |> rlang::parse_exprs()
 
-  # select a subset of columns - change dat to .data
+  # select a minimum subset of columns
   date_lookup <- .data |>
     dplyr::select(pol_num, !!!date_cols)
 
-
-  # add dates to transaction data
-  trx_data <- dplyr::left_join(
-    trx_data, date_lookup, multiple = "error",
-    dplyr::join_by(pol_num,
-                   between(trx_date, !!date_cols[[1]], !!date_cols[[2]]))) |>
+  # add dates to transaction data and pivot / summarize to match the grain
+  #   of exposure data
+  trx_data <- trx_data |>
+    # column renames
+    dplyr::rename(pol_num = {{col_pol_num}},
+                  trx_date = {{col_trx_date}},
+                  trx_type = {{col_trx_type}},
+                  trx_amt = {{col_trx_amt}}) |>
+    # between join by transaction date falling within exposures windows
+    dplyr::left_join(
+      date_lookup, multiple = "error",
+      dplyr::join_by(pol_num,
+                     between(trx_date, !!date_cols[[1]], !!date_cols[[2]]))) |>
     dplyr::mutate(trx_n = 1) |>
     tidyr::pivot_wider(
       names_from = trx_type,
