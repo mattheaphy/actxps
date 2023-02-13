ee4580838a624dba694abf289879bc877df5ca2c
Matt Heaphy <matt.heaphy@gmail.com>
2023-02-13 06:51:43 -0500

    expanded trx_stats tests

--- tests/testthat/test-trx_stats.R
+++ tests/testthat/test-trx_stats.R
@@ -1,14 +1,46 @@
-expo <- expose_py(census_dat, "2019-12-31", target_status = "Surrender")
-expo2 <- add_transactions(expo, withdrawals)
+no_trx <- expose_py(census_dat, "2019-12-31", target_status = "Surrender")
+expo <- expose_py(census_dat, "2019-12-31", target_status = "Surrender") |>
+  add_transactions(withdrawals) |>
+  dplyr::left_join(account_vals, by = c("pol_num", "pol_date_yr"))
+
+res <- expo |>
+  dplyr::group_by(pol_yr, inc_guar) |>
+  trx_stats(percent_of = c("av_anniv", "premium"))
 
 test_that("trx_stats error checks work", {
 
   expect_error(trx_stats(1))
-  expect_error(trx_stats(expo))
-  expect_error(trx_stats(expo2, trx_types = c("abc", "def")))
-  expect_no_error(trx_stats(expo2))
+  expect_error(trx_stats(no_trx))
+  expect_error(trx_stats(expo, trx_types = c("abc", "def")))
+  expect_no_error(trx_stats(expo))
 
 })
 
+test_that("Experience study summary method checks", {
+  expect_identical(res, summary(res, pol_yr, inc_guar))
+  expect_equal(trx_stats(expo, percent_of = c("av_anniv", "premium")),
+               summary(res))
+})
 
-# TODO make sure to test that summary is equivalent to calling trx_stats w/o groups
+expo2 <- head(expo) |> dplyr::rename(ex = exposure)
+
+test_that("Renaming works", {
+  expect_error(trx_stats(expo2))
+  expect_no_error(trx_stats(expo2, col_exposure = "ex"))
+})
+
+test_that("trx_stats works", {
+
+  expect_equal(ncol(expo |> trx_stats(trx_types = "Base",
+                                      percent_of = c("av_anniv", "premium"))),
+               ncol(res) - 2)
+  expect_s3_class(res, "trx_df")
+  expect_true(all(res$exposure >= res$trx_flag, na.rm = TRUE))
+  expect_true(all(res$avg_trx >= res$avg_all, na.rm = TRUE))
+  expect_true(all(res$trx_freq >= res$trx_util, na.rm = TRUE))
+  expect_true(all(res$pct_of_av_anniv_w_trx >= res$pct_of_av_anniv_all,
+                  na.rm = TRUE))
+  expect_true(all(trx_stats(expo)$exposure <=
+                    trx_stats(expo, full_exposures_only = FALSE)$exposure))
+
+})
