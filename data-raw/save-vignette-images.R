688388d400850a5df1b510b493247d90e69b62a9
Matt Heaphy <matt.heaphy@gmail.com>
2023-08-09 00:16:00 -0400

    - Removed creation of gt image on the fly in the transactions vignette to avoid an RMD check error. - The code to generate the required image was moved to a separate script

---
+++ data-raw/save-vignette-images.R
@@ -0,0 +1,22 @@
+# Purpose: this script is used to create images used by vignettes for gt tables
+
+
+# Transaction tables example ----------------------------------------------
+
+library(actxps)
+
+exposed_data <- expose_py(census_dat, "2019-12-31",
+                          target_status = "Surrender") |>
+  add_transactions(withdrawals) |>
+  left_join(account_vals, by = c("pol_num", "pol_date_yr"))
+trx_res <- exposed_data |>
+  group_by(pol_yr, inc_guar) |>
+  trx_stats(percent_of = "av_anniv")
+
+trx_res |>
+  # remove periods with zero transactions
+  filter(trx_n > 0) |>
+  # first 10 rows showed for brevity
+  head(10) |>
+  autotable() |>
+  gt::gtsave("man/figures/trx_gt.png")
