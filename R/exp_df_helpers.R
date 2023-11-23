ee98ecb9e0fee5781fd3e63da71629f0402c9c6f
Matt Heaphy <matt.heaphy@gmail.com>
2023-11-23 18:03:31 -0500

    weight supported added to as_exp_df

--- R/exp_df_helpers.R
+++ R/exp_df_helpers.R
@@ -7,16 +7,52 @@
 #' `as_exp_df()` will coerce a data frame to an `exp_df` object if that
 #' data frame has columns for exposures and claims.
 #'
-#' `as_exp_df()` is most useful for converting existing aggregate summaries of
+#' `as_exp_df()` is most useful for working with existing aggregate summaries of
 #' experience where individual policy information is not available.
+#'
+#' If nothing is passed to `wt`, the data frame `x` must include columns
+#' containing:
+#'
+#' - Exposures (`exposure`)
+#' - Claim counts (`claims`)
+#'
+#' If `wt` is passed, the data must include columns containing:
+#'
+#' - Weighted exposures (`exposure`)
+#' - Weighted claims (`claims`)
+#' - Claim counts (`n_claims`)
+#' - The raw sum of weights **NOT** multiplied by exposures
+#' - Exposure record counts (`.weight_n`)
+#' - The raw sum of squared weights (`.weight_sq`)
+#'
+#' The names in parentheses above are expected column names. If the data
+#' frame passed to `as_exp_df()` uses different column names, these can be
+#' specified using the `col_*` arguments.
+#'
+#' When a column name is passed to `wt`, the columns `.weight`, `.weight_n`,
+#' and `.weight_sq` are used to calculate credibility and confidence intervals.
+#' If credibility and confidence intervals aren't required, then it is not
+#' necessary to pass anything to `wt`. The results of `as_exp_df()` and any
+#' downstream summaries will still be weighted as long as the exposures and
+#' claims are pre-weighted.
 #'
 #' @param x An object. For `as_exp_df()`, `x` must be a data frame.
 #' @param expected A character vector containing column names in x with
 #' expected values
+#' @param wt Optional. Length 1 character vector. Name of the column in `x`
+#' containing weights to use in the calculation of claims, exposures, partial
+#' credibility, and confidence intervals.
 #' @param col_claims Optional. Name of the column in `x` containing claims. The
 #' assumed default is "claims".
 #' @param col_exposure Optional. Name of the column in `x` containing exposures.
 #' The assumed default is "exposure".
+#' @param col_n_claims Optional and only used used when `wt` is passed. Name of
+#' the column in `x` containing the number of claims.
+#' @param col_weight_sq Optional and only used used when `wt` is passed. Name of
+#' the column in `x` containing the sum of squared weights (also not multiplied
+#' by exposures).
+#' @param col_weight_n Optional and only used used when `wt` is passed. Name of
+#' the column in `x` containing exposure record counts.
 #' @param credibility If `TRUE`, future calls to [summary()] will include
 #' partial credibility weights and credibility-weighted termination rates.
 #' @param conf_level Confidence level used for the Limited Fluctuation
@@ -34,7 +70,9 @@
 #' @seealso exp_stats
 #'
 #' @export
-as_exp_df <- function(x, expected = NULL, col_claims, col_exposure,
+as_exp_df <- function(x, expected = NULL, wt = NULL,
+                      col_claims, col_exposure,
+                      col_n_claims, col_weight_sq, col_weight_n,
                       target_status = NULL,
                       start_date = NULL, end_date = NULL,
                       credibility = FALSE,
@@ -50,12 +88,22 @@ as_exp_df <- function(x, expected = NULL, col_claims, col_exposure,
   if (!missing(col_exposure)) x <- x |> rename(exposure = {{col_exposure}})
   if (!missing(col_claims)) x <- x |> rename(claims = {{col_claims}})
 
+  if (is.null(wt)) {
+    req_names <- c("exposure", "claims")
+  } else {
+    req_names <- c("exposure", "claims", "n_claims", ".weight",
+                   ".weight_sq", ".weight_n")
+    if (!missing(col_n_claims)) x <- x |> rename(n_claims = {{col_n_claims}})
+    x <- x |> rename(.weight = {{wt}})
+    if (!missing(col_weight_sq)) x <- x |>
+        rename(.weight_sq = {{col_weight_sq}})
+    if (!missing(col_weight_n)) x <- x |> rename(.weight_n = {{col_weight_n}})
+  }
+
   # check required columns
-  # pol_num, status, exposure, 2 date cols, policy period (policy expo only)
-  req_names <- c("exposure", "claims")
   verify_col_names(names(x), req_names)
 
-  x <- x |> mutate(n_claims = claims)
+  if (is.null(wt)) x$n_claims <- x$claims
 
   new_exp_df(x,
              .groups = list(),
@@ -63,7 +111,7 @@ as_exp_df <- function(x, expected = NULL, col_claims, col_exposure,
              start_date = start_date,
              expected = expected,
              end_date = end_date,
-             wt = NULL,
+             wt = wt,
              credibility = credibility,
              conf_level = conf_level, cred_r = cred_r,
              conf_int = conf_int)
