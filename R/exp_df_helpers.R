1fea4864ba74304b87ea10bc95efcddd2b8ca9b2
Matt Heaphy <matt.heaphy@gmail.com>
2023-11-23 09:34:28 -0500

    as_exp_df checking, tests, and doc updates

--- R/exp_df_helpers.R
+++ R/exp_df_helpers.R
@@ -1,16 +1,61 @@
-#' Exposed data frame helper functions
+#' Termination summary helper functions
+#'
+#' Test for and coerce to the `exp_df` class.
+#'
+#' `is_exp_df()` will return `TRUE` if `x` is an `exp_df` object.
+#'
+#' `as_exp_df()` will coerce a data frame to an `exp_df` object if that
+#' data frame has columns for exposures and claims.
+#'
+#' `as_exp_df()` is most useful for converting existing aggregate summaries of
+#' experience where individual policy information is not available.
+#'
+#' @param x An object. For `as_exp_df()`, `x` must be a data frame.
+#' @param expected A character vector containing column names in x with
+#' expected values
+#' @param col_claims Optional. Name of the column in `x` containing claims. The
+#' assumed default is "claims".
+#' @param col_exposure Optional. Name of the column in `x` containing exposures.
+#' The assumed default is "exposure".
+#' @param credibility If `TRUE`, future calls to [summary()] will include
+#' partial credibility weights and credibility-weighted termination rates.
+#' @param conf_level Confidence level used for the Limited Fluctuation
+#' credibility method and confidence intervals
+#' @param cred_r Error tolerance under the Limited Fluctuation credibility
+#' method
+#' @param conf_int If `TRUE`, future calls to [summary()] will include
+#' confidence intervals around the observed termination rates and any
+#' actual-to-expected ratios.
+#' @inheritParams expose
+#'
+#' @return For `is_exp_df()`, a length-1 logical vector. For `as_exp_df()`,
+#' an `exp_df` object.
+#'
+#' @seealso exp_stats
+#'
 #' @export
-as_exp_df <- function(x, expected = NULL, col_claims = "claims",
-                      col_exposure = "exposure",
+as_exp_df <- function(x, expected = NULL, col_claims, col_exposure,
                       target_status = NULL,
                       start_date = NULL, end_date = NULL,
                       credibility = FALSE,
                       conf_level = 0.95, cred_r = 0.05, conf_int = FALSE) {
 
-  x <- x |>
-    rename(exposure = {{col_exposure}},
-           claims = {{col_claims}}) |>
-    mutate(n_claims = claims)
+  if (is_exp_df(x)) return(x)
+
+  if (!is.data.frame(x)) {
+    rlang::abort("`x` must be a data frame.")
+  }
+
+  # column name alignment
+  if (!missing(col_exposure)) x <- x |> rename(exposure = {{col_exposure}})
+  if (!missing(col_claims)) x <- x |> rename(claims = {{col_claims}})
+
+  # check required columns
+  # pol_num, status, exposure, 2 date cols, policy period (policy expo only)
+  req_names <- c("exposure", "claims")
+  verify_col_names(names(x), req_names)
+
+  x <- x |> mutate(n_claims = claims)
 
   new_exp_df(x,
              .groups = list(),
---
+++ tests/testthat/test-exp_df_helpers.R
@@ -0,0 +1,35 @@
+res <- expose(toy_census, "2022-12-31", target_status = "Surrender") |>
+  exp_stats()
+
+test_that("is_exp_df works", {
+  expect_true(is_exp_df(res))
+  expect_false(is_exp_df(mtcars))
+})
+
+res2 <- as.data.frame(res)
+
+test_that("as_exp_df works", {
+
+
+  res3 <- as_exp_df(res2)
+  res4 <- res2 |>
+    rename(expo = exposure)
+  res5 <- res4 |>
+    rename(clms = claims)
+
+  expect_error(as_exp_df(data.frame(a = 1:3), Sys.Date()),
+               regexp = "The following columns are missing")
+
+  expect_true(is_exp_df(as_exp_df(res)))
+
+  expect_false(is_exp_df(res2))
+
+  expect_true(is_exp_df(res3))
+
+  expect_error(as_exp_df(res4, regexp = "The following columns are missing"))
+  expect_no_error(as_exp_df(res4, col_exposure = "expo"))
+  expect_no_error(as_exp_df(res5, col_exposure = "expo", col_claims = "clms"))
+
+  expect_error(as_exp_df(1), regexp = "`x` must be a data frame.")
+
+})
